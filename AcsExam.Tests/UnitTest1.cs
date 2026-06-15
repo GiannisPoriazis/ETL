fd958411de6dd86452c6ed5aad3d933439673566
Ioannis Poriazis <ioannis.poriazis@gmail.com>
2026-06-15 14:15:34 +0300

    Initialize project files

---
+++ AcsExam.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace AcsExam.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ AcsExam/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AcsExam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
